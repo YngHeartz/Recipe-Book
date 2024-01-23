589f0877b4e2a7fcfb9e7f076499b4f4500090bb
Daniel Hill <108755211+YngHeartz@users.noreply.github.com>
2024-01-23 17:08:08 -0500

    added some logic for the add recipe button

--- src/components/recipeinput.jsx
+++ src/components/recipeinput.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function RecipeInput(){
+
+    const [recipe, setRecipe] = useState([]);
+    
+    const AddRecipe = (e) => {
+        e.preventDefault();
+        setRecipe(e.target.value);
+    }
 
     return(
         <>
         <div className="text-center mt-20">
             <input type="text" placeholder="Recipe Name" className="size-9 w-auto text-center border-s" />
-            <button className="ml-10 text-white p-2 bg-black rounded-sm hover:bg-pink-300 hover:text-white hover:font-bold transition duration-500">Add Recipe</button>
+            <button className="ml-10 text-white p-2 bg-black rounded-sm hover:bg-pink-300 hover:text-white hover:font-bold transition duration-500" onClick={AddRecipe}>Add Recipe</button>
         </div>
         </>
     );
