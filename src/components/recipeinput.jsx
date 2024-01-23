445cde45c6582b626397f848382938a660e682ee
Daniel Hill <108755211+YngHeartz@users.noreply.github.com>
2024-01-23 16:50:25 -0500

    extra line gone

--- src/components/recipeinput.jsx
+++ src/components/recipeinput.jsx
@@ -8,7 +8,6 @@ function RecipeInput(){
             <input type="text" placeholder="Recipe Name" className="size-9 w-auto text-center border-s" />
             <button className="ml-10 text-white p-2 bg-black rounded-sm hover:bg-pink-300 hover:text-white hover:font-bold transition duration-500">Add Recipe</button>
         </div>
-            
         </>
     );
 }
